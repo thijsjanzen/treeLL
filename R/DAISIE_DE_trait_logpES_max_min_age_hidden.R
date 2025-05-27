f6b81d31f3deef79d396d93383192c37cb3a6240
O. Dehayem Nanwou <o.d.nanwou@rug.nl>
2025-05-27 23:03:07 +0200

    update function

--- R/DAISIE_DE_trait_logpES_max_min_age_hidden.R
+++ R/DAISIE_DE_trait_logpES_max_min_age_hidden.R
@@ -49,7 +49,6 @@ DAISIE_DE_trait_logpES_max_min_age_hidden <- function(brts,
                                                   trait,
                                                   sf = 1,
                                                   parameter,
-                                                  sf = 1,
                                                   num_observed_states,
                                                   num_hidden_states,
                                                   atol = 1e-10,
@@ -65,74 +64,7 @@ DAISIE_DE_trait_logpES_max_min_age_hidden <- function(brts,
 
   #########interval2 [t_p, tmin]
 
-  interval2 <- function(t, state, parameter) {
-    with(as.list(c(state, parameter)), {
-
-      lambdac <- parameter[[1]]
-      mu      <- parameter[[2]]
-      gamma   <- parameter[[3]]
-      lambdaa <- parameter[[4]]
-      q       <- parameter[[5]]
-      p       <- parameter[[6]]
-
-
-      # n <- (length(state) - 1) / 4
-      n <- num_observed_states * num_hidden_states
-
-      dDE     <- numeric(n)
-      dDM2    <- numeric(n)
-      dDM3    <- numeric(n)
-      dE      <- numeric(n)
-
-      t_vec   <- rowSums(q)
-
-      DE  <- state[1:n]
-      DM2 <- state[(n + 1):(n + n)]
-      DM3 <- state[(n + n + 1):(n + n + n)]
-      E   <- state[(n + n + n + 1):(n + n + n + n)]
-      DA3 <- state[length(state)]
-
-      gamma_matrix <- matrix(parameter[[3]], nrow = n, ncol = length(parameter[[3]]), byrow = TRUE)
-
-      if (nrow(gamma_matrix) == 1) {
-        # when there's only one row, there is no “self” element to subtract
-        gamma_nonself <- 0
-      } else {
-        # for n > 1, subtract the diagonal (self‐effects) as before
-        gamma_nonself <- rowSums(gamma_matrix - diag(parameter[[3]]))
-      }
-
-      q_mult_E   <- t(q %*% E)
-      q_mult_DE  <- t(q %*% DE)
-      q_mult_DM2 <- t(q %*% DM2)
-      q_mult_DM3 <- t(q %*% DM3)
-
-
-      dDE <- -(lambdac + mu + t_vec) * DE +
-        2 * lambdac * DE * E +
-        q_mult_DE
-
-
-      dDM2 <- -(lambdac + mu + gamma + lambdaa + t_vec) * DM2 +
-        (lambdaa * DE + 2 * lambdac * DE * E + p * q_mult_DE) * DA3 +
-        (1 - p) * q_mult_DM2 + gamma_nonself * DM2
-
-
-
-      dDM3 <-  -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM3 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA3 +
-        (1 - p) * q_mult_DM3 +
-        gamma_nonself * DM3
-
-      dE <- mu - (mu + lambdac + t_vec) * E +
-        lambdac * E * E +
-        q_mult_E
-
-      dDA3 <- -sum(gamma) * DA3 + sum(gamma * DM3)
-
-      return(list(c(dDE, dDM2, dDM3, dE, dDA3)))
-    })
-  }
+  interval2 <- get_func_interval(interval ="interval2")
 
 
   m = length(parameter[[1]])
@@ -180,100 +112,23 @@ DAISIE_DE_trait_logpES_max_min_age_hidden <- function(brts,
                             rtol = rtol)
 
 
-  solution2 <- matrix(solution1[,-1], nrow = 2) # remove the time from the result
+  solution2 <- matrix(solution2[,-1], nrow = 2) # remove the time from the result
 
   #########interval3 [tmin, tmax]
 
 
-  interval3 <- function(t, state, parameter) {
-    with(as.list(c(state, parameter)), {
-
-      lambdac <- parameter[[1]]
-      mu      <- parameter[[2]]
-      gamma   <- parameter[[3]]
-      lambdaa <- parameter[[4]]
-      q       <- parameter[[5]]
-      p       <- parameter[[6]]
-
-
-      # n <- (length(state) - 1) / 4
-      n <- num_observed_states*num_hidden_states
-      dDE     <- numeric(n)
-      dDM1    <- numeric(n)
-      dDM2    <- numeric(n)
-      dDM3    <- numeric(n)
-      dE      <- numeric(n)
-
-      t_vec   <- rowSums(q)
-
-      DE  <- state[1:n]
-      DM1 <- state[(n + 1):(n + n)]
-      DM2 <- state[(n + n + 1):(n + n + n)]
-      DM3 <- state[(n + n + n + 1):(n + n + n + n)]
-      E   <- state[(n + n + n + n + 1):(n + n + n + n + n)]
-      DA2 <- state[length(state) - 1]
-      DA3 <- state[length(state)]
-
-      gamma_matrix <- matrix(parameter[[3]], nrow = n, ncol = length(parameter[[3]]), byrow = TRUE)
-
-      if (nrow(gamma_matrix) == 1) {
-        # when there's only one row, there is no “self” element to subtract
-        gamma_nonself <- 0
-      } else {
-        # for n > 1, subtract the diagonal (self‐effects) as before
-        gamma_nonself <- rowSums(gamma_matrix - diag(parameter[[3]]))
-      }
-
-      q_mult_E   <- t(q %*% E)
-      q_mult_DE  <- t(q %*% DE)
-      q_mult_DM1 <- t(q %*% DM1)
-      q_mult_DM2 <- t(q %*% DM2)
-      q_mult_DM3 <- t(q %*% DM3)
-
-
-      dDE <- -(lambdac + mu + t_vec) * DE +
-        2 * lambdac * DE * E +
-        q_mult_DE
-
-      dDM1 <- -(lambdac + mu + gamma + lambdaa + t_vec) * DM1 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA2 +
-        (1 - p) * q_mult_DM1 + gamma * DM2
-
-
-      dDM2 <- -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM2 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA2 +
-        (lambdaa * DE + 2*lambdac * DE + p * q_mult_DE) * DA3 +
-        (1 - p) * q_mult_DM2 + gamma_nonself * DM2
-
-
-
-      dDM3 <-  -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM3 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA3 +
-        (1 - p) * q_mult_DM3 +
-        gamma_nonself * DM3
-
-      dE <- mu - (mu + lambdac + t_vec) * E +
-        lambdac * E * E +
-        q_mult_E
-
-      dDA2 <- -sum(gamma) * DA2 + sum(gamma * DM2)
-
-      dDA3 <- -sum(gamma) * DA3 + sum(gamma * DM3)
-
-      return(list(c(dDE, dDM1, dDM2, dDM3, dE, dDA2, dDA3)))
-    })
-  }
+  interval3 <- get_func_interval(interval ="interval3")
 
   # Initial conditions
 
   # only use second row, because the first row of solution3 is the initial state
-  initial_conditions3_max_min <- c(solution1[2,][1:n],                                             ### DE: select DE in solution2
+  initial_conditions3_max_min <- c(solution2[2,][1:n],                                             ### DE: select DE in solution2
                            rep(0, n),                                                      ### DM1: select DE in solution2
                            solution2[2,][(n + 1):(n + n)],                         ### DM2: select DM2 in solution2
                            solution2[2,][(n + n + 1):(n + n + n)],                 ### DM3: select DM3 in solution2
                            solution2[2,][(n + n + n + 1):(n + n + n + n)],         ### E: select E in solution2
                            0,                                                              ### DA2
-                           solution2[2,][length(solution1[2,])])                           ### DA3: select DA3 in solution2
+                           solution2[2,][length(solution2[2,])])                           ### DA3: select DA3 in solution2
 
   initial_conditions3_max_min <- matrix(initial_conditions3_max_min, nrow = 1)
 
@@ -294,56 +149,7 @@ DAISIE_DE_trait_logpES_max_min_age_hidden <- function(brts,
 
   #########interval4 [tmax, t0]
 
-  interval4 <- function(t, state, parameter) {
-    with(as.list(c(state, parameter)), {
-
-      lambdac <- parameter[[1]]
-      mu      <- parameter[[2]]
-      gamma   <- parameter[[3]]
-      lambdaa <- parameter[[4]]
-      q       <- parameter[[5]]
-      p       <- parameter[[6]]
-
-
-      #n <- (length(state) - 1) / 2
-      n <- num_observed_states * num_hidden_states
-
-      dDM1    <- numeric(n)
-      dDE     <- numeric(n)
-
-
-      t_vec   <- rowSums(q)
-
-      DM1  <- state[1:n]
-      E    <- state[(n + 1):(n + n)]
-      DA1  <- state[length(state)]
-
-      gamma_matrix <- matrix(parameter[[3]], nrow = n, ncol = length(parameter[[3]]), byrow = TRUE)
-
-      if (nrow(gamma_matrix) == 1) {
-        # when there's only one row, there is no “self” element to subtract
-        gamma_nonself <- 0
-      } else {
-        # for n > 1, subtract the diagonal (self‐effects) as before
-        gamma_nonself <- rowSums(gamma_matrix - diag(parameter[[3]]))
-      }
-
-      q_mult_E   <- t(q %*% E)
-      q_mult_DM1 <- t(q %*% DM1)
-
-      dDM1 <-  -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM1 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA1 +
-        (1 - p) * q_mult_DM1 + gamma_nonself * DM1
-
-      dE <- mu - (mu + lambdac + t_vec) * E +
-        lambdac * E * E +
-        q_mult_E
-
-      dDA1 <- -sum(gamma) * DA1 + sum(gamma * DM1)
-
-      return(list(c(dDM1, dE, dDA1)))
-    })
-  }
+  interval4 <- get_func_interval(interval ="interval4")
 
 
   # Initial conditions
--- R/DAISIE_DE_trait_logpNE_max_min_age_hidden.R
+++ R/DAISIE_DE_trait_logpNE_max_min_age_hidden.R
@@ -61,74 +61,7 @@ DAISIE_DE_trait_logpNE_max_min_age_hidden <- function(brts,
 
   #########interval2 [t_p, tmin]
 
-  interval2 <- function(t, state, parameter) {
-    with(as.list(c(state, parameter)), {
-
-      lambdac <- parameter[[1]]
-      mu      <- parameter[[2]]
-      gamma   <- parameter[[3]]
-      lambdaa <- parameter[[4]]
-      q       <- parameter[[5]]
-      p       <- parameter[[6]]
-
-
-      # n <- (length(state) - 1) / 4
-      n <- num_observed_states * num_hidden_states
-
-      dDE     <- numeric(n)
-      dDM2    <- numeric(n)
-      dDM3    <- numeric(n)
-      dE      <- numeric(n)
-
-      t_vec   <- rowSums(q)
-
-      DE  <- state[1:n]
-      DM2 <- state[(n + 1):(n + n)]
-      DM3 <- state[(n + n + 1):(n + n + n)]
-      E   <- state[(n + n + n + 1):(n + n + n + n)]
-      DA3 <- state[length(state)]
-
-      gamma_matrix <- matrix(parameter[[3]], nrow = n, ncol = length(parameter[[3]]), byrow = TRUE)
-
-      if (nrow(gamma_matrix) == 1) {
-        # when there's only one row, there is no “self” element to subtract
-        gamma_nonself <- 0
-      } else {
-        # for n > 1, subtract the diagonal (self‐effects) as before
-        gamma_nonself <- rowSums(gamma_matrix - diag(parameter[[3]]))
-      }
-
-      q_mult_E   <- t(q %*% E)
-      q_mult_DE  <- t(q %*% DE)
-      q_mult_DM2 <- t(q %*% DM2)
-      q_mult_DM3 <- t(q %*% DM3)
-
-
-      dDE <- -(lambdac + mu + t_vec) * DE +
-        2 * lambdac * DE * E +
-        q_mult_DE
-
-
-      dDM2 <- -(lambdac + mu + gamma + lambdaa + t_vec) * DM2 +
-        (lambdaa * DE + 2 * lambdac * DE * E + p * q_mult_DE) * DA3 +
-        (1 - p) * q_mult_DM2 + gamma_nonself * DM2
-
-
-
-      dDM3 <-  -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM3 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA3 +
-        (1 - p) * q_mult_DM3 +
-        gamma_nonself * DM3
-
-      dE <- mu - (mu + lambdac + t_vec) * E +
-        lambdac * E * E +
-        q_mult_E
-
-      dDA3 <- -sum(gamma) * DA3 + sum(gamma * DM3)
-
-      return(list(c(dDE, dDM2, dDM3, dE, dDA3)))
-    })
-  }
+  interval2 <- get_func_interval(interval ="interval2")
 
 
   m = length(parameter[[1]])
@@ -183,84 +116,7 @@ DAISIE_DE_trait_logpNE_max_min_age_hidden <- function(brts,
   #########interval3 [tmin, tmax]
 
 
-  interval3 <- function(t, state, parameter) {
-    with(as.list(c(state, parameter)), {
-
-      lambdac <- parameter[[1]]
-      mu      <- parameter[[2]]
-      gamma   <- parameter[[3]]
-      lambdaa <- parameter[[4]]
-      q       <- parameter[[5]]
-      p       <- parameter[[6]]
-
-
-      # n <- (length(state) - 1) / 4
-      n <- num_observed_states*num_hidden_states
-      dDE     <- numeric(n)
-      dDM1    <- numeric(n)
-      dDM2    <- numeric(n)
-      dDM3    <- numeric(n)
-      dE      <- numeric(n)
-
-      t_vec   <- rowSums(q)
-
-      DE  <- state[1:n]
-      DM1 <- state[(n + 1):(n + n)]
-      DM2 <- state[(n + n + 1):(n + n + n)]
-      DM3 <- state[(n + n + n + 1):(n + n + n + n)]
-      E   <- state[(n + n + n + n + 1):(n + n + n + n + n)]
-      DA2 <- state[length(state) - 1]
-      DA3 <- state[length(state)]
-
-      gamma_matrix <- matrix(parameter[[3]], nrow = n, ncol = length(parameter[[3]]), byrow = TRUE)
-
-      if (nrow(gamma_matrix) == 1) {
-        # when there's only one row, there is no “self” element to subtract
-        gamma_nonself <- 0
-      } else {
-        # for n > 1, subtract the diagonal (self‐effects) as before
-        gamma_nonself <- rowSums(gamma_matrix - diag(parameter[[3]]))
-      }
-
-      q_mult_E   <- t(q %*% E)
-      q_mult_DE  <- t(q %*% DE)
-      q_mult_DM1 <- t(q %*% DM1)
-      q_mult_DM2 <- t(q %*% DM2)
-      q_mult_DM3 <- t(q %*% DM3)
-
-
-      dDE <- -(lambdac + mu + t_vec) * DE +
-        2 * lambdac * DE * E +
-        q_mult_DE
-
-      dDM1 <- -(lambdac + mu + gamma + lambdaa + t_vec) * DM1 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA2 +
-        (1 - p) * q_mult_DM1 + gamma * DM2
-
-
-      dDM2 <- -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM2 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA2 +
-        (lambdaa * DE + 2*lambdac * DE + p * q_mult_DE) * DA3 +
-        (1 - p) * q_mult_DM2 + gamma_nonself * DM2
-
-
-
-      dDM3 <-  -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM3 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA3 +
-        (1 - p) * q_mult_DM3 +
-        gamma_nonself * DM3
-
-      dE <- mu - (mu + lambdac + t_vec) * E +
-        lambdac * E * E +
-        q_mult_E
-
-      dDA2 <- -sum(gamma) * DA2 + sum(gamma * DM2)
-
-      dDA3 <- -sum(gamma) * DA3 + sum(gamma * DM3)
-
-      return(list(c(dDE, dDM1, dDM2, dDM3, dE, dDA2, dDA3)))
-    })
-  }
+  interval3 <- get_func_interval(interval ="interval3")
 
   # Initial conditions
 
@@ -292,56 +148,7 @@ DAISIE_DE_trait_logpNE_max_min_age_hidden <- function(brts,
 
   #########interval4 [tmax, t0]
 
-  interval4 <- function(t, state, parameter) {
-    with(as.list(c(state, parameter)), {
-
-      lambdac <- parameter[[1]]
-      mu      <- parameter[[2]]
-      gamma   <- parameter[[3]]
-      lambdaa <- parameter[[4]]
-      q       <- parameter[[5]]
-      p       <- parameter[[6]]
-
-
-      #n <- (length(state) - 1) / 2
-      n <- num_observed_states * num_hidden_states
-
-      dDM1    <- numeric(n)
-      dDE     <- numeric(n)
-
-
-      t_vec   <- rowSums(q)
-
-      DM1  <- state[1:n]
-      E    <- state[(n + 1):(n + n)]
-      DA1  <- state[length(state)]
-
-      gamma_matrix <- matrix(parameter[[3]], nrow = n, ncol = length(parameter[[3]]), byrow = TRUE)
-
-      if (nrow(gamma_matrix) == 1) {
-        # when there's only one row, there is no “self” element to subtract
-        gamma_nonself <- 0
-      } else {
-        # for n > 1, subtract the diagonal (self‐effects) as before
-        gamma_nonself <- rowSums(gamma_matrix - diag(parameter[[3]]))
-      }
-
-      q_mult_E   <- t(q %*% E)
-      q_mult_DM1 <- t(q %*% DM1)
-
-      dDM1 <-  -(lambdac + mu + gamma_nonself + lambdaa + t_vec) * DM1 +
-        (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA1 +
-        (1 - p) * q_mult_DM1 + gamma_nonself * DM1
-
-      dE <- mu - (mu + lambdac + t_vec) * E +
-        lambdac * E * E +
-        q_mult_E
-
-      dDA1 <- -sum(gamma) * DA1 + sum(gamma * DM1)
-
-      return(list(c(dDM1, dE, dDA1)))
-    })
-  }
+  interval4 <- get_func_interval(interval ="interval4")
 
 
   # Initial conditions
