7541bb2e53406e9df3a6a3bfd486f15cf8761596
Ornela Dehayem Nanwou <o.dehayem@gmail.com>
2025-07-07 13:40:55 +0200

    added sampling fraction as argument

--- R/get_initial_conditions.R
+++ R/get_initial_conditions.R
@@ -7,7 +7,7 @@ get_initial_conditions2 <- function(status,
                                     num_observed_states,
                                     num_hidden_states,
                                     brts,
-                                    sampling_fraction,
+                                    sampling_fraction = c(1),
                                     trait_mainland_ancestor) {
   n <- num_observed_states * num_hidden_states
   num_unique_states <- n
@@ -35,11 +35,11 @@ get_initial_conditions2 <- function(status,
     return(matrix(initial_conditions2, nrow = 1))
   } else if (status == 2 && length(brts) == 2) {
 
+    # secret assumption of trait being a single value
     if (length(trait) > 1) {
-      stop("status == 2 assumes trait is not a vector, but received a vector")
+      stop("status == 2 assumes trait to be single value, found vector")
     }
-
-    if (all(is.na(trait))) {
+    if (is.na(trait)) {
       s <- c()
       for (i in seq_along(sampling_fraction)) {
         s <- c(s, rep(sampling_fraction[i], num_hidden_states))
@@ -47,48 +47,51 @@ get_initial_conditions2 <- function(status,
       DE[1:n] <- s
       E[1:n]  <- 1 - s
     } else {
-      DE[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
-      E[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)]  <- 1 - sampling_fraction
+      DE[c((num_hidden_states * trait + 1),
+           num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
+      E[c((num_hidden_states * trait + 1),
+          num_hidden_states + trait * num_hidden_states)] <-
+                                                           1 - sampling_fraction
     }
 
-  }
-  else if (status == 3 && length(brts) == 2 ) {
-    DE[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
-    E[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- 1 - sampling_fraction
+  } else if (status == 3 && length(brts) == 2) {
+    DE[c((num_hidden_states * trait + 1),
+         num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
+    E[c((num_hidden_states * trait + 1),
+        num_hidden_states + trait * num_hidden_states)] <- 1 - sampling_fraction
     DM3[c((num_hidden_states * trait_mainland_ancestor + 1),
           num_hidden_states + trait_mainland_ancestor * num_hidden_states)] <- 1
-  }
-  else if (status == 4) {
+  } else if (status == 4) {
+
     if (length(trait) > 1) {
-      stop("status == 4 assumes trait is not a vector, but received a vector")
+      stop("status == 4 assumes trait to be single value, found vector")
     }
 
-    if ( is.na(trait) ) {
+    if (is.na(trait)) {
       DM2[1:n] <- 1
-    }
-    else {
-      DM2[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- 1
+    } else {
+      DM2[c((num_hidden_states * trait + 1),
+            num_hidden_states + trait * num_hidden_states)] <- 1
     }
 
-  }
-  else if(status == 8) {
+  } else if (status == 8) {
     if (length(trait) > 1) {
-      stop("status == 8 assumes trait is not a vector, but received a vector")
+      stop("status == 8 assumes trait to be single value, found vector")
     }
+
     if (is.na(trait)) {
       s <- c()
       for (i in seq_along(sampling_fraction)) {
         s <- c(s, rep(sampling_fraction[i], num_hidden_states))
       }
       DM2[1:n] <- s
+    } else {
+      DM2[c((num_hidden_states * trait + 1),
+            num_hidden_states + trait * num_hidden_states)] <- 1
     }
-    else {
-      DM2[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- 1
-    }
-  }
-  else if(status == 9)  {
+  } else if (status == 9)  {
     if (length(trait) > 1) {
-      stop("status == 9 assumes trait is not a vector, but received a vector")
+      stop("status == 9 assumes trait to be single value, found vector")
     }
 
     if (is.na(trait)) {
@@ -98,10 +101,12 @@ get_initial_conditions2 <- function(status,
       }
       DE[1:n] <- s
       E[1:n]  <- 1 - s
-    }
-    else  {
-      DE[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
-      E[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)]  <- 1 - sampling_fraction
+    } else  {
+      DE[c((num_hidden_states * trait + 1),
+           num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
+      E[c((num_hidden_states * trait + 1),
+          num_hidden_states + trait * num_hidden_states)]  <-
+                                                           1 - sampling_fraction
     }
   }
 
@@ -140,8 +145,9 @@ get_initial_conditions3 <- function(status,
 
   if (status == 1) {
     if (length(trait) > 1) {
-      stop("status == 1 assumes trait is not a vector, but received a vector")
+      stop("status == 1 assumes trait to be single value, found vector")
     }
+
 
     if (is.na(trait)) {
       s <- c()
@@ -149,9 +155,9 @@ get_initial_conditions3 <- function(status,
         s <- c(s, rep(sampling_fraction[i], num_hidden_states))
       }
       DM2[1:n] <- s
-    }
-    else if (trait == trait) {
-      DM2[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
+    } else if (trait == trait) {
+      DM2[c((num_hidden_states * trait + 1),
+            num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
     }
 
     initial_conditions3 <- c(DE, DM1, DM2, DM3, E, DA2, DA3)
@@ -163,23 +169,23 @@ get_initial_conditions3 <- function(status,
                              res[(n + n + 1):(n + n + n)],                          ## E
                              0,                                                     ## DA2
                              res[length(res)])                                      ## DA3
-  }
-  else if (status == 5) {
+  } else if (status == 5) {
     if (length(trait) > 1) {
-      stop("status == 5 assumes trait is not a vector, but received a vector")
+      stop("status == 5 assumes trait to be single value, found vector")
     }
 
-    if (all(is.na(trait))) {
+    if (is.na(trait)) {
       s <- c()
       for (i in seq_along(sampling_fraction)) {
         s <- c(s, rep(sampling_fraction[i], num_hidden_states))
       }
       DE[1:n] <- s
       E[1:n]  <- 1 - s
-    }
-    else  {
-      DE[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
-      E[c((num_hidden_states * trait + 1), num_hidden_states + trait * num_hidden_states)]  <- 1 - sampling_fraction
+    } else  {
+      DE[c((num_hidden_states * trait + 1),
+           num_hidden_states + trait * num_hidden_states)] <- sampling_fraction
+      E[c((num_hidden_states * trait + 1),
+          num_hidden_states + trait * num_hidden_states)]  <- 1 - sampling_fraction
     }
 
     initial_conditions3 <- c(DE, DM1, DM2, DM3, E, DA2, DA3)
@@ -214,31 +220,30 @@ get_initial_conditions4 <- function(status,
       dist_gamma <- dist_gamma_tma(parameter[[3]],
                                    trait_mainland_ancestor,
                                    n)
-      initial_conditions4 <- c(rep( sum(dist_gamma * (solution[2,][(n + 1):(n + n)])), n), ### DM1: select DM2 in solution2
-                               solution[2,][(n + n + n + 1):(n + n + n + n)],         ### E: select E in solution2
-                               sum(dist_gamma * (solution[2,][(n + 1):(n + n)])))          ### DA1: select DM2 in solution2
-    }else{
+      initial_conditions4 <- c(rep(sum(dist_gamma * (solution[2, ][(n + 1):(n + n)])), n), ### DM1: select DM2 in solution2
+                               solution[2, ][(n + n + n + 1):(n + n + n + n)],         ### E: select E in solution2
+                               sum(dist_gamma * (solution[2, ][(n + 1):(n + n)])))          ### DA1: select DM2 in solution2
+    } else {
       #if the trait state of the species at the stem is known
 
       dist_gamma <- dist_gamma_tma(parameter[[3]],
                                    trait_mainland_ancestor,
                                    n)
 
-      initial_conditions4 <- c(rep(sum( dist_gamma* (solution[2, ][(n + 1):(n + n)])), n ), ### DM1: select DM2 in solution2
-                               solution[2,][(n + n + n + 1):(n + n + n + n)],                                                                       ### E: select E in solution2
-                               sum( dist_gamma* (solution[2, ][(n + 1):(n + n)])))          ### DA1: select DM2 in solution2
+      initial_conditions4 <- c(rep(sum(dist_gamma * (solution[2, ][(n + 1):(n + n)])), n), ### DM1: select DM2 in solution2
+                               solution[2, ][(n + n + n + 1):(n + n + n + n)],                                                                       ### E: select E in solution2
+                               sum(dist_gamma * (solution[2, ][(n + 1):(n + n)])))          ### DA1: select DM2 in solution2
     }
   } else if (status == 1 || status == 5 || status == 6) {
-    initial_conditions4 <- c(solution[2,][(n + 1):(n + n)],                                 ### DM1: select DM1 in solution1
-                             solution[2,][(n + n + n + n + 1):(n + n + n + n + n)],         ### E: select E in solution1
-                             solution[2,][length(solution[2, ]) - 1])                        ### DA1: select DA2 in solution1
+    initial_conditions4 <- c(solution[2, ][(n + 1):(n + n)],                                 ### DM1: select DM1 in solution1
+                             solution[2, ][(n + n + n + n + 1):(n + n + n + n + n)],         ### E: select E in solution1
+                             solution[2, ][length(solution[2, ]) - 1])                        ### DA1: select DA2 in solution1
 
   } else if (status == 8 || status == 9) {
-    initial_conditions4 <- c(solution[2,][(n + 1):(n + n)],                                 ### DM1: select DM2 in solution3
-                             solution[2,][(n + n + n + n + 1):(n + n + n + n + n)],         ### E: select E in solution3
-                             solution[2,][length(solution[2, ]) - 1])                        ### DA1: select DA2 in solution3
+    initial_conditions4 <- c(solution[2, ][(n + 1):(n + n)],                                 ### DM1: select DM2 in solution3
+                             solution[2, ][(n + n + n + n + 1):(n + n + n + n + n)],         ### E: select E in solution3
+                             solution[2, ][length(solution[2, ]) - 1])                       ### DA1: select DA2 in solution3
 
   }
   return(matrix(initial_conditions4, nrow = 1))
 }
-
