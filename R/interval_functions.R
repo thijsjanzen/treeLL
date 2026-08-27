598e8d5866df9ec83c7fd1bec654c9dcab65573f
Ornela Dehayem <o.dehayem@gmail.com>
2026-08-27 13:58:35 +0200

    turned into a matrix to allow different p values for transitions between different trait states

--- R/interval_functions.R
+++ R/interval_functions.R
@@ -160,8 +160,6 @@ compute_mainland_weights <- function(Mp,
 }
 #############
 
-
-
 #' @keywords internal
 interval2 <- function(t, state, parameter) {
   with(as.list(c(state, parameter)), {
@@ -172,51 +170,55 @@ interval2 <- function(t, state, parameter) {
     q       <- parameter[[5]]
     p       <- parameter[[6]]
     trait_mainland_ancestor <- parameter[[7]]
-
     n <- (length(state) - 1) / 4
+
+    # p must match q's dimensions, with the diagonal fixed at 1
+    if (!all(dim(p) == dim(q))) {
+      stop("p must have the same dimensions as q")
+    }
+    if (!all(diag(p) == 1)) {
+      stop("the diagonal of p must be filled with 1")
+    }
+
+    # element-wise combination of p and q (p[i,j] always pairs with q[i,j])
+    pq  <- p * q
+    opq <- (1 - p) * q
 
     dDE     <- numeric(n)
     dDM2    <- numeric(n)
     dDM3    <- numeric(n)
     dE      <- numeric(n)
-
     t_vec <- rowSums(q)
-
     DE  <- state[1:n]
     DM2 <- state[(n + 1):(n + n)]
     DM3 <- state[(n + n + 1):(n + n + n)]
     E   <- state[(n + n + n + 1):(n + n + n + n)]
     DA3 <- state[length(state)]
-
     q_mult_E   <- t(q %*% E)
     q_mult_DE  <- t(q %*% DE)
     q_mult_DM2 <- t(q %*% DM2)
     q_mult_DM3 <- t(q %*% DM3)
-
-
+    pq_mult_E  <- t(pq %*% E)
+    pq_mult_DE <- t(pq %*% DE)
+    opq_mult_DM2 <- t(opq %*% DM2)
+    opq_mult_DM3 <- t(opq %*% DM3)
     # TODO: pass trait mainland_ancestor to interval functions
     # trait mainland ancestor is vector of probabilities
-
     dist_gamma <- dist_gamma_tma(gamma,
                                  trait_mainland_ancestor,
                                  n)
-
     dDE <- -(lambdac + mu + t_vec) * DE +
       2 * lambdac * DE * E +
       q_mult_DE
-
     dDM2 <- -(lambdac + mu + sum(dist_gamma) + lambdaa + t_vec) * DM2 +
-      (lambdaa * DE + 2 * lambdac * DE * E + p * q_mult_DE) * DA3 +
-      (1 - p) * q_mult_DM2
-
+      (lambdaa * DE + 2 * lambdac * DE * E + pq_mult_DE) * DA3 +
+      opq_mult_DM2
     dDM3 <- -(lambdac + mu + lambdaa + t_vec) * DM3 +
-      (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA3 +
-      (1 - p) * q_mult_DM3
-
+      (mu + lambdaa * E + lambdac * E * E + pq_mult_E) * DA3 +
+      opq_mult_DM3
     dE <- mu - (mu + lambdac + t_vec) * E +
       lambdac * E * E +
       q_mult_E
-
     dDA3 <- -sum(dist_gamma) * DA3 + sum(dist_gamma * DM3)
     return(list(c(dDE, dDM2, dDM3, dE, dDA3)))
   })
@@ -232,17 +234,26 @@ interval3 <- function(t, state, parameter) {
     q       <- parameter[[5]]
     p       <- parameter[[6]]
     trait_mainland_ancestor <- parameter[[7]]
-
     n <- (length(state) - 2) / 5
+
+    # p must match q's dimensions, with the diagonal fixed at 1
+    if (!all(dim(p) == dim(q))) {
+      stop("p must have the same dimensions as q")
+    }
+    if (!all(diag(p) == 1)) {
+      stop("the diagonal of p must be filled with 1")
+    }
+
+    # element-wise combination of p and q (p[i,j] always pairs with q[i,j])
+    pq  <- p * q
+    opq <- (1 - p) * q
 
     dDE     <- numeric(n)
     dDM1    <- numeric(n)
     dDM2    <- numeric(n)
     dDM3    <- numeric(n)
     dE      <- numeric(n)
-
     t_vec <- rowSums(q)
-
     DE  <- state[1:n]
     DM1 <- state[(n + 1):(n + n)]
     DM2 <- state[(n + n + 1):(n + n + n)]
@@ -250,46 +261,40 @@ interval3 <- function(t, state, parameter) {
     E   <- state[(n + n + n + n + 1):(n + n + n + n + n)]
     DA2 <- state[length(state) - 1]
     DA3 <- state[length(state)]
-
     q_mult_E   <- t(q %*% E)
     q_mult_DE  <- t(q %*% DE)
     q_mult_DM1 <- t(q %*% DM1)
     q_mult_DM2 <- t(q %*% DM2)
     q_mult_DM3 <- t(q %*% DM3)
-
+    pq_mult_E    <- t(pq %*% E)
+    pq_mult_DE   <- t(pq %*% DE)
+    opq_mult_DM1 <- t(opq %*% DM1)
+    opq_mult_DM2 <- t(opq %*% DM2)
+    opq_mult_DM3 <- t(opq %*% DM3)
     dist_gamma <- dist_gamma_tma(gamma,
                                  trait_mainland_ancestor,
                                  n)
-
-
     dDE <- -(lambdac + mu + t_vec) * DE +
       2 * lambdac * DE * E +
       q_mult_DE
-
     dDM1 <- -(lambdac + mu + sum(dist_gamma) + lambdaa + t_vec) * DM1 +
-      (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA2 +
-      (1 - p) * q_mult_DM1 + sum(dist_gamma * DM2)
-
+      (mu + lambdaa * E + lambdac * E * E + pq_mult_E) * DA2 +
+      opq_mult_DM1 + sum(dist_gamma * DM2)
     dDM2 <- -(lambdac + mu + lambdaa + t_vec) * DM2 +
-      (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA2 +
-      (lambdaa * DE + 2 * lambdac * DE * E + p * q_mult_DE) * DA3 +
-      (1 - p) * q_mult_DM2
-
+      (mu + lambdaa * E + lambdac * E * E + pq_mult_E) * DA2 +
+      (lambdaa * DE + 2 * lambdac * DE * E + pq_mult_DE) * DA3 +
+      opq_mult_DM2
     dDM3 <- -(lambdac + mu + lambdaa + t_vec) * DM3 +
-      (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA3 +
-      (1 - p) * q_mult_DM3
-
+      (mu + lambdaa * E + lambdac * E * E + pq_mult_E) * DA3 +
+      opq_mult_DM3
     dE <- mu - (mu + lambdac + t_vec) * E +
       lambdac * E * E +
       q_mult_E
-
     dDA2 <- -sum(dist_gamma) * DA2 + sum(dist_gamma * DM2)
     dDA3 <- -sum(dist_gamma) * DA3 + sum(dist_gamma * DM3)
-
     return(list(c(dDE, dDM1, dDM2, dDM3, dE, dDA2, dDA3)))
   })
 }
-
 #' @keywords internal
 interval4 <- function(t, state, parameter) {
   with(as.list(c(state, parameter)), {
@@ -300,37 +305,40 @@ interval4 <- function(t, state, parameter) {
     q       <- parameter[[5]]
     p       <- parameter[[6]]
     trait_mainland_ancestor <- parameter[[7]]
-
     n <- (length(state) - 1) / 2
+
+    # p must match q's dimensions, with the diagonal fixed at 1
+    if (!all(dim(p) == dim(q))) {
+      stop("p must have the same dimensions as q")
+    }
+    if (!all(diag(p) == 1)) {
+      stop("the diagonal of p must be filled with 1")
+    }
+
+    # element-wise combination of p and q (p[i,j] always pairs with q[i,j])
+    pq  <- p * q
+    opq <- (1 - p) * q
 
     dDM1 <- numeric(n)
     dDE  <- numeric(n)
-
     t_vec <- rowSums(q)
-
     DM1 <- state[1:n]
     E   <- state[(n + 1):(n + n)]
     DA1 <- state[length(state)]
-
     q_mult_E   <- t(q %*% E)
     q_mult_DM1 <- t(q %*% DM1)
-
-
+    pq_mult_E    <- t(pq %*% E)
+    opq_mult_DM1 <- t(opq %*% DM1)
     dist_gamma <- dist_gamma_tma(gamma,
                                  trait_mainland_ancestor,
                                  n)
-
     dDM1 <- -(lambdac + mu + lambdaa + t_vec) * DM1 +
-      (mu + lambdaa * E + lambdac * E * E + p * q_mult_E) * DA1 +
-      (1 - p) * q_mult_DM1
-
+      (mu + lambdaa * E + lambdac * E * E + pq_mult_E) * DA1 +
+      opq_mult_DM1
     dE <- mu - (mu + lambdac + t_vec) * E +
       lambdac * E * E +
       q_mult_E
-
     dDA1 <- -sum(dist_gamma) * DA1 + sum(dist_gamma * DM1)
-
     return(list(c(dDM1, dE, dDA1)))
   })
 }
-
